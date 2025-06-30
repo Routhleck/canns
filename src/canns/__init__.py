b5478058af73cd4186216f71fec29aaf1c694d4d
routhleck <sichaohe@stu.pku.edu.cn>
2025-06-30 13:11:56 +0800

    Refactor core application structure
    Restructures the main application modules including the analyzer, models, pipeline, task, and trainer. This change improves modularity and clarifies the overall data flow. Also updates .gitignore.

--- src/canns/__init__.py
+++ src/canns/__init__.py
@@ -2,4 +2,8 @@ __all__ = (  # noqa: F405
     # TODO: Add all public symbols here.
 )
 
-from .canns import *  # noqa: F403
+from .models import Models
+from .trainer import Trainer
+from .pipeline import Pipeline
+from .task import Task
+from .analyzer import Analyzer
